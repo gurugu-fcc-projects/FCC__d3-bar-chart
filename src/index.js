07a1a409fcbc268f051557db8f789908b8bbd58e
GuRuGu <peter.k.nets@gmail.com>
2020-02-19 11:43:54 +0200

    Display loaded data items

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ const dataUrl =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
 d3.json(dataUrl)
-  .then(data => console.log(data))
+  .then(({ data }) => {
+    console.log(data);
+    d3.select("#main")
+      .selectAll("div")
+      .data(data)
+      .enter()
+      .append("div")
+      .text(d => `${d[0]} --- ${d[1]}`);
+  })
   .catch(err => console.error(err));
