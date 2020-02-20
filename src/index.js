8a71f4204697e9acfef36cc13239ae9b12d137dc
GuRuGu <peter.k.nets@gmail.com>
2020-02-20 17:59:34 +0200

    Show simple bars

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ d3.json(dataUrl)
       .data(data)
       .enter()
       .append("div")
-      .text(d => `${d[0]} --- ${d[1]}`);
+      .text(d => `${d[0]} --- ${d[1]}`)
+      // .style("font-family", "vardana")
+      // .style("color", d => {
+      //   const year = d[0].split("-")[0];
+
+      //   return year > 1960 ? "mediumspringgreen" : "azure";
+      // })
+      // .attr("class", d => (d[1] > 1000 ? "high" : ""));
+      .attr("class", "bar")
+      .style("width", d => `${d[1]}px`);
   })
   .catch(err => console.error(err));
