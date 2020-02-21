bb00dd80a85f34a538ec9783a5a8ba5d95fbcef3
GuRuGu <peter.k.nets@gmail.com>
2020-02-21 09:21:51 +0200

    Show inverted svg bars

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 const dataUrl =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
+const w = 600;
+const h = 1000;
+
+const svg = d3
+  .select("#main")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h);
+
 d3.json(dataUrl)
   .then(({ data }) => {
-    d3.select("#main")
-      .selectAll("div")
+    console.log(data);
+
+    svg
+      .selectAll("rect")
       .data(data)
       .enter()
-      .append("div")
-      // .text(d => `${d[0]} --- ${d[1]}`)
-      // .style("font-family", "vardana")
-      // .style("color", d => {
-      //   const year = d[0].split("-")[0];
+      .append("rect")
+      .attr("x", (d, i) => i * 3)
+      .attr("y", 0)
+      .attr("width", 2)
+      .attr("height", (d, i) => d[1]);
+    // .text(d => `${d[0]} --- ${d[1]}`)
+    // .style("font-family", "vardana")
+    // .style("color", d => {
+    //   const year = d[0].split("-")[0];
 
-      //   return year > 1960 ? "mediumspringgreen" : "azure";
-      // })
-      // .attr("class", d => (d[1] > 1000 ? "high" : ""));
-      .attr("class", "bar")
-      .style("height", d => `${d[1] * 5}px`);
+    //   return year > 1960 ? "mediumspringgreen" : "azure";
+    // })
+    // .attr("class", d => (d[1] > 1000 ? "high" : ""));
+    // .attr("class", "bar")
+    // .style("height", d => `${d[1] * 5}px`);
   })
   .catch(err => console.error(err));
