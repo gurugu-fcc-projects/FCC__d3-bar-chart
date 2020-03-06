057efbcb6b1b7b687194a5b9aee620bc79bbe7e7
GuRuGu <peter.k.nets@gmail.com>
2020-03-06 09:20:34 +0200

    Adjust graph and y-axis

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const dataUrl =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-const w = 500;
-const h = 400;
+const w = 630;
+const h = 350;
+const padding = 50;
 
 const svg = d3
   .select("#main")
@@ -15,23 +16,27 @@ d3.json(dataUrl)
     // console.log(data);
 
     const max = d3.max(data, d => d[1]);
-    const min = d3.min(data, d => d[1]);
+    // const min = d3.min(data, d => d[1]);
 
     const yScale = d3
       .scaleLinear()
       .domain([0, max])
-      .range([0, h]);
+      .range([h - padding, padding]);
+
+    const yAxis = d3.axisLeft(yScale);
 
     svg
       .selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => i * 1.5)
-      .attr("y", (d, i) => h - yScale(d[1]))
-      .attr("width", 1.5)
-      .attr("height", (d, i) => yScale(d[1]))
+      .attr("x", (d, i) => i * 2 + padding)
+      .attr("y", (d, i) => yScale(d[1]))
+      .attr("width", 2)
+      .attr("height", (d, i) => h - yScale(d[1]) - padding)
       .attr("class", "bar")
+      .attr("data-date", (d, i) => d[0])
+      .attr("data-gdp", (d, i) => d[1])
       .append("title")
       .text(d => d[1]);
 
@@ -40,9 +45,14 @@ d3.json(dataUrl)
       .data(data)
       .enter()
       .append("text")
-      .attr("x", (d, i) => i * 3)
-      .attr("y", (d, i) => h - 20 - d[1] / 3)
+      .attr("x", (d, i) => i * 2)
+      .attr("y", (d, i) => h - 20 - yScale(d[1]))
       .attr("class", "bar-text")
       .text(d => d[0]);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(${padding - 5}, 0)`)
+      .call(yAxis);
   })
   .catch(err => console.error(err));
