086b725ba2330c4d16fe59d11631ee7fb81c315b
GuRuGu <peter.k.nets@gmail.com>
2020-03-19 15:56:09 +0200

    Show proper Y axis ticks

--- src/index.js
+++ src/index.js
@@ -1,46 +1,69 @@
 const dataUrl =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-const w = 630;
-const h = 350;
-const padding = 50;
+const margin = { top: 20, right: 20, bottom: 70, left: 40 };
+const width = 1200 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+const parseDate = d3.isoParse;
+
+const x = d3
+  .scaleBand()
+  .rangeRound([0, width], 0.05)
+  .padding(0.1);
+
+const y = d3.scaleLinear().range([height, 0]);
+
+const xAxis = d3
+  .axisBottom()
+  .scale(x)
+  .tickFormat(d3.timeFormat("%b"));
+
+const yAxis = d3
+  .axisLeft()
+  .scale(y)
+  .ticks(10);
 
 const svg = d3
-  .select("#main")
+  .select(".main")
   .append("svg")
-  .attr("width", w)
-  .attr("height", h);
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.json(dataUrl)
   .then(({ data }) => {
+    data.forEach(d => {
+      d[0] = parseDate(d[0]);
+      d[1] = +d[1];
+    });
+
+    x.domain(data.map(d => d[0]));
+    y.domain([0, d3.max(data, d => d[1])]);
+
     const maxGDP = d3.max(data, d => d[1]);
-    // const years = data.map(datum => Number(datum[0].split("-")[0]));
-    const years = data.map(datum => new Date(datum[0].split("-")[0]));
-    const minYear = d3.min(years, year => year);
-    const maxYear = d3.max(years, year => year);
+    const barWidth = width / data.length;
 
     const yScale = d3
       .scaleLinear()
       .domain([0, maxGDP])
-      .range([h - padding, padding]);
+      .range([height, 0]);
 
     const xScale = d3
-      .scaleLinear()
-      .domain([minYear, maxYear])
-      .range([padding, w - padding]);
-
-    const yAxis = d3.axisLeft(yScale);
-    const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y"));
+      .scaleTime()
+      .domain(d3.extent(data, d => new Date(d[0])))
+      .range([0, width]);
 
     svg
       .selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => i * 2 + padding)
+      .attr("x", (d, i) => xScale(new Date(d[0])))
       .attr("y", (d, i) => yScale(d[1]))
-      .attr("width", 2)
-      .attr("height", (d, i) => h - yScale(d[1]) - padding)
+      .attr("width", barWidth)
+      .attr("height", (d, i) => height - yScale(d[1]))
       .attr("class", "bar")
       .attr("data-date", (d, i) => d[0])
       .attr("data-gdp", (d, i) => d[1])
@@ -53,18 +76,18 @@ d3.json(dataUrl)
       .enter()
       .append("text")
       .attr("x", (d, i) => i * 2)
-      .attr("y", (d, i) => h - 20 - yScale(d[1]))
+      .attr("y", (d, i) => height - 20 - yScale(d[1]))
       .attr("class", "bar-text")
       .text(d => d[0]);
 
     svg
       .append("g")
-      .attr("transform", `translate(${padding}, 0)`)
+      .attr("transform", `translate(0, 0)`)
       .call(yAxis);
 
     svg
       .append("g")
-      .attr("transform", `translate(0, ${h - padding})`)
+      .attr("transform", `translate(0, ${height})`)
       .call(xAxis);
   })
   .catch(err => console.error(err));
