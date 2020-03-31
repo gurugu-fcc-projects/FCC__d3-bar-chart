cdfcd52d9e34d78e47c12180c33d08c9e27be360
GuRuGu <peter.k.nets@gmail.com>
2020-03-31 14:36:15 +0300

    Increase tick label font-size

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const dataUrl =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+const margin = { top: 20, right: 20, bottom: 50, left: 60 };
 const width = 800 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
